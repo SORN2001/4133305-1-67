11415e651dca3be3d570b7a6bf47b43df4a6c344
SORN2001 <son.danuson@gmail.com>
2024-06-11 16:39:50 +0700

    LAB

---
+++ lab2/pages/back.js
@@ -0,0 +1,11 @@
+export default function back(){
+    return(
+        <button onClick={api()}>Call APT</button>
+    )
+}
+
+async function api(){
+    const res = await fetch("http://127.0.0.1:3000/api/hello")
+    const data = res.json()
+    console.log(data);
+}
